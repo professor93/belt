6ec98149c194fbd61f4517682160b486d844b1b6
Sodiqmirzo Sattorov <mr.sodiqmirzosattorov17@gmail.com>
2022-10-21 16:42:05 +0500

    proxy added

--- config/belt.php
+++ config/belt.php
@@ -5,8 +5,8 @@ return [
     'base_url' => env('BELT_BASE_URL', 'https://belt.uz/api/v1/'),
     'username' => env('BELT_USERNAME', 'username'),
     'password' => env('BELT_PASSWORD', 'password'),
-    'proxy_url' => env('MYID_PROXY_URL'),
-    'proxy_proto' => env('MYID_PROXY_PROTO'),
-    'proxy_host' => env('MYID_PROXY_HOST'),
-    'proxy_port' => env('MYID_PROXY_PORT'),
+    'proxy_url' => env('BELT_PROXY_URL'),
+    'proxy_proto' => env('BELT_PROXY_PROTO'),
+    'proxy_host' => env('BELT_PROXY_HOST'),
+    'proxy_port' => env('BELT_PROXY_PORT'),
 ];
