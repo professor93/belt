d92988b5b954005f7f3e4467cab1863348070b15
Sodiqmirzo Sattorov <mr.sodiqmirzosattorov17@gmail.com>
2022-10-21 17:04:07 +0500

    removed proxy

--- config/belt.php
+++ config/belt.php
@@ -5,8 +5,4 @@ return [
     'base_url' => env('BELT_BASE_URL', 'https://belt.uz/api/v1/'),
     'username' => env('BELT_USERNAME', 'username'),
     'password' => env('BELT_PASSWORD', 'password'),
-    'proxy_url' => env('BELT_PROXY_URL'),
-    'proxy_proto' => env('BELT_PROXY_PROTO'),
-    'proxy_host' => env('BELT_PROXY_HOST'),
-    'proxy_port' => env('BELT_PROXY_PORT'),
 ];
--- src/Belt.php
+++ src/Belt.php
@@ -9,12 +9,7 @@ class Belt
     private function sendRequest(string $method, string $url, array $data = [])
     {
         $config = config('belt');
-        $options = [];
-        $proxy_url = $config['proxy_url'] ?? (($config['proxy_proto'] ?? '') . '://' . ($config['proxy_host'] ?? '') . ':' . ($config['proxy_port'] ?? '')) ?? '';
-        if (is_string($proxy_url) && str_contains($proxy_url, '://') && strlen($proxy_url) > 12) {
-            $options['proxy'] = $proxy_url;
-        }
-        return Http::baseUrl($config['base_url'])->withOptions($options)
+        return Http::baseUrl($config['base_url'])
             ->withBasicAuth($config['username'], $config['password'])->$method($url, $data)->json();
     }
 
